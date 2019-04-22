1730869dc80178dfa8c3dd1f1456ef59b61a3bec
Mateus Inocencio Maia <mateus_maaia@hotmail.com>
2019-04-22 14:40:26 -0300

    Setting mandatory params

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var program = require('commander')
 
 program
   .version('1.0.0', '-v, --version')
-  .option('-b, --bits <n>', 'The Encrypion bits that will be used (Eg. 4096)', parseInt)
-  .option('-s, --string <string>', 'The string that you want to encrypt/decrypt')
+  .option('-b, --bits <n>', '[Obrigatory] The Encrypion bits that will be used (Eg. 4096)', parseInt)
+  .option('-s, --string <string>', '[Obrigatory] The string that you want to encrypt/decrypt')
   .parse(process.argv)
 
-rsaEncryption(program.string, program.bits)
+if (!program.bits || !program.string) {
+  program.outputHelp()
+} else {
+  rsaEncryption(program.string, program.bits)
+}
